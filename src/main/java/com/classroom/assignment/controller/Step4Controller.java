132e314ac6a7411a45ef1ecf71198868a58dac08
st323a2110a-kita <146682631+st323a2110a-kita@users.noreply.github.com>
2023-10-04 02:55:51 +0000

    Co-authored-by: st323a2127m-hayashi <st323a2127m-hayashi@users.noreply.github.com> Co-authored-by: st323a2124k-nakamura <st323a2124k-nakamura@users.noreply.github.com> Co-authored-by: st323a2105h-ooe <st323a2105h-ooe@users.noreply.github.com>

--- src/main/java/com/classroom/assignment/controller/Step4Controller.java
+++ src/main/java/com/classroom/assignment/controller/Step4Controller.java
@@ -22,6 +22,9 @@ public class Step4Controller {
     studentList.add(new Student("321A0003", "佐藤 次郎"));
     // 課題 level2 step4
     // 所属学科を追加してください。
+    studentList.add(new Student("321A0001", "山田 太郎", "プログラミング専攻"));
+    studentList.add(new Student("321A0002", "田中 一郎", "ロボット・Iot専攻"));
+    studentList.add(new Student("321A0003", "佐藤 次郎", "クラウド専攻"));
 
     model.addAttribute("studentList", studentList);
 
